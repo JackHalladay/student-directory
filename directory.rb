f175eedef031eb47436659c80a54cc861d69200d
Jack <jackhalladay@gmail.com>
2021-07-14 17:14:18 +0100

    Added cohorts to students using an array of hashes

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
 #students in an array
-students = ["Dr. Hannibal Lecter", 
-  "Darth Vader", 
-  "Nurse Ratched", 
-  "Michael Corleone", 
-  "Alex DeLarge", 
-  "The Wicked Witch Of The West", 
-  "Terminator", 
-  "Freddy Kruger", 
-  "The Joker", 
-  "Joffery Baratheon", 
-  "Norman Bates"]
+students = [ 
+  {name: "Dr. Hannibal Lecter", cohort: "november"},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch Of The West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Kruger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november} 
+]
 
 #here we print the header
 def print_header
@@ -18,9 +20,9 @@ def print_header
 end
 
 #prints the students
-def print_students(names)
-  names.each do |name|
-    puts name
+def prints(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 #prints the footer
@@ -29,5 +31,5 @@ def print_footer(names)
 end
 
 print_header
-print_students(students)
+prints(students)
 print_footer(students)
